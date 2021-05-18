b560fd88ddd8d71a5a2a78dc27b4512927739dcd
rafabusleme <raabusleme@uc.cl>
2021-05-18 16:40:31 -0400

    :rocket: update main component

---
+++ src/components/FlightsInfo/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FlightsInfo() {
+  return <div>Im data info</div>;
+}
+
+export default FlightsInfo;
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const ENDPOINT = 'wss://tarea-3-websocket.2021-1.tallerdeintegracion.cl';
