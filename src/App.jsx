52b7e67dc2b6eb9b666a0b7887b1aca86b9790e1
rafabusleme <raabusleme@uc.cl>
2021-05-19 20:03:48 -0400

    define main socket

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 
+import { io } from 'socket.io-client';
+
 import Chat from './components/Chat';
 import FlightsInfo from './components/FlightsInfo';
+// eslint-disable-next-line import/order
 import FlightTracker from './components/FlightTracker/';
 
 import './App.css';
 
+import { ENDPOINT } from './constants';
+
 function App() {
+  const socket = io(ENDPOINT, {
+    path: '/flights/',
+    transports: ['websocket'],
+  });
+
   return (
     <div className="app-container">
       <div className="map-container">
-        <FlightTracker />
-        <FlightsInfo />
+        <FlightTracker socket={socket} />
+        <FlightsInfo socket={socket} />
       </div>
       <div className="chat-container">
-        <Chat />
+        <Chat socket={socket} />
       </div>
     </div>
   );
